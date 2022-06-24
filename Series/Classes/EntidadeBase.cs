fb35bbf78a699568da87d817f70d8860800cd99a
Douglas Rezende <douglasrezende198@gmail.com>
2022-06-24 01:27:16 -0300

    Projeto

---
+++ Series/Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+﻿namespace Series
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
---
+++ Series/Classes/Serie.cs
@@ -0,0 +1,54 @@
+﻿namespace Series
+{
+    internal class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            Id = id;
+            Genero = genero;
+            Titulo = titulo;
+            Descricao = descricao;
+            Ano = ano;
+            Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+
+            retorno += "Gênero: " + Genero + Environment.NewLine;
+            retorno += "Título: " + Titulo + Environment.NewLine;
+            retorno += "Descrição: " + Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + Ano + Environment.NewLine;
+            retorno += "Excluído: " + Excluido;
+
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return Titulo;
+        }
+
+        public int retornaId()
+        {
+            return Id;
+        }
+
+        public bool retornaExcluido()
+        {
+            return Excluido;
+        }
+        
+        public void Excluir()
+        {
+            Excluido = true;
+        }
+    }
+}
